bf47c278a5b097be68073c78522c51c57a57b382
sergey144010 <sergey144010@mail.ru>
2016-03-31 23:39:36 +0800

    Исправлено создание файлов конфига

--- create_config.php
+++ create_config.php
@@ -1,23 +1,27 @@
 <?php
 class Config
 {
+    public $config = "config.php";
+    public $configNew = "config_new.php";
+    public $configBase64 = "config_base64_encode.txt";
+
     public function encode()
     {
         // Получаем закодированную строку
-        $current = file_get_contents("config.php");
+        $current = file_get_contents($this->config);
         $current = base64_encode($current);
-        file_put_contents("config_base64_encode.txt", $current);
+        file_put_contents($this->configBase64, $current);
     }
     public function create()
     {
         // Создаем файл конфига
-        $current = file_get_contents("config_base64_encode.txt");
+        $current = file_get_contents($this->configBase64);
         $current = base64_decode($current);
-        if(is_file("config.php"))
+        if(is_file($this->config))
         {
-            file_put_contents("config_new.php", $current);
+            file_put_contents($this->configNew, $current);
         }else{
-            file_put_contents("config.php", $current);
+            file_put_contents($this->config, $current);
         };
     }
 }
